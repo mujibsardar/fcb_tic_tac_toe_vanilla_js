f61e21859a59f3efd5112b665486ba84e9bd3a91
Avan <mujibsardar@gmail.com>
2020-10-15 13:29:55 -0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -12,5 +12,25 @@ document.querySelectorAll('.board-cell').forEach(item => {
       current_mark = current_mark == x ? o : x;
       element.innerHTML = current_mark;
     }
+
+    if (didWin()) {
+      alert(current_mark + " won!")
+    }
+
   })
 });
+
+// Checks if there is a winner
+function didWin(){
+  // Check if all content in row 1 are the same 1 2 3 => 6
+  // Check if all content in row 2 are the same 4 5 6 => 15
+  // Check if all content in row 3 are the same 7 8 9 => 24
+
+  // Check if all content in column 1 are the same 1 4 7 => 12
+  // Check if all content in column 2 are the same 2 5 8 => 15
+  // Check if all content in column 3 are the same 3 6 9 => 18
+
+  // Check if all content in diagonal 1 are the same 1 5 9 => 15
+  // Check if all content in diagonal 2 are the same 3 5 7 => 15
+  return false;
+}
